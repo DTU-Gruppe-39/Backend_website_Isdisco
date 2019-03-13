0ca8f7699c9d418aea19bbdb158afce398c9fc1a
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-03-13 13:41:17 +0100

    Create User.cs
    Simple base User class.

---
+++ IsDisco/IsDisco/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsDisco.Models
+{
+    public class User
+    {
+        string name;
+        int ID;
+        Boolean VIP;
+  
+        public User(string name, int ID)
+        {
+            this.name = name;
+            this.ID = ID;
+            VIP = false;
+        }
+
+        private void SetVIP()
+        {
+            VIP = true;
+        }
+        
+    }
+}
