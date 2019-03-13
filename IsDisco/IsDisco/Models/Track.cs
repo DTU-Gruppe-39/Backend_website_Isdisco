8f7a39d0c17c24e02d1b8a0913f48768ddc914ba
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-03-13 14:22:07 +0100

    Update Track.cs
    Co-Authored-By: cilian <cilian@users.noreply.github.com>

--- IsDisco/IsDisco/Models/Track.cs
+++ IsDisco/IsDisco/Models/Track.cs
@@ -1,28 +1,34 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
+namespace IsDisco.Models
+{
+    public class Track
+    {
 
-public class Track {
+        private List<User> users { get; set; }
+        private String song { get; set; }
+        private String author { get; set; }
+        private String id_link { get; set; }
+        private DateTime since { get; set; }
+        private int votes { get; set; }
+        private int cd_timer { get; set; }
+        private Boolean cooldown { get; set; }
+        private Boolean blacklist { get; set; }
 
-    private ArrayList<User> users {get; set;} 
-    private String song {get; set;} 
-    private String author {get; set;}
-    private String id_link {get; set;}
-    private DateTime since {get; set;} 
-    private int votes {get; set;} 
-    private int cd_timer {get; set;}
-    private boolean cooldown {get; set;}
-    private boolean blacklist {get; set;} 
-
-    public Track(String id_link, ArrayList<User> users, String song, String author, 
-    DateTime since, int votes, boolean cooldown, int cd_timer, boolean blacklist){
-        this.id_link = id_link;
-        this.users = users;
-        this.song = song;
-        this.author = author;
-        this.since = since;
-        this.votes = votes;
-        this.cooldown = cooldown;
-        this.cd_timer = cd_timer;
-        this.blacklist = blacklist;
+        public Track(String id_link, List<User> users, String song, String author,
+        DateTime since, int votes, Boolean cooldown, int cd_timer, Boolean blacklist)
+        {
+            this.id_link = id_link;
+            this.users = users;
+            this.song = song;
+            this.author = author;
+            this.since = since;
+            this.votes = votes;
+            this.cooldown = cooldown;
+            this.cd_timer = cd_timer;
+            this.blacklist = blacklist;
+        }
     }
 }
