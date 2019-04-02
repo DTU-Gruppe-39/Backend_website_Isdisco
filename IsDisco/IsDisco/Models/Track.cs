0ac889ec60c1d697dabd32fa6742645a249ce7a3
Thomas Mattsson <tmattsson0@gmail.com>
2019-04-02 17:33:49 +0200

    Livefeed works

--- IsDisco/IsDisco/Models/Track.cs
+++ IsDisco/IsDisco/Models/Track.cs
@@ -8,28 +8,28 @@ namespace IsDisco.Models
 {
     public class Track
     {
-        public List<User> users { get; set; }
-        public string song { get; set; }
-        public string author { get; set; }
-        public string id_link { get; set; }
-        public DateTime since { get; set; }
-        public int votes { get; set; }
-        public int cd_timer { get; set; }
-        public Boolean cooldown { get; set; }
-        public Boolean blacklist { get; set; }
+        public List<User> Users { get; set; }
+        public string Song { get; set; }
+        public string Author { get; set; }
+        public string Id_link { get; set; }
+        public DateTime Since { get; set; }
+        public int Votes { get; set; }
+        public int Cd_timer { get; set; }
+        public Boolean Cooldown { get; set; }
+        public Boolean Blacklist { get; set; }
 
         public Track(string id_link, List<User> users, string song, string author,
         DateTime since, int votes, Boolean cooldown, int cd_timer, Boolean blacklist)
         {
-            this.id_link = id_link;
-            this.users = users;
-            this.song = song;
-            this.author = author;
-            this.since = since;
-            this.votes = votes;
-            this.cooldown = cooldown;
-            this.cd_timer = cd_timer;
-            this.blacklist = blacklist;
+            this.Id_link = id_link;
+            this.Users = users;
+            this.Song = song;
+            this.Author = author;
+            this.Since = since;
+            this.Votes = votes;
+            this.Cooldown = cooldown;
+            this.Cd_timer = cd_timer;
+            this.Blacklist = blacklist;
         }
     }
 }
