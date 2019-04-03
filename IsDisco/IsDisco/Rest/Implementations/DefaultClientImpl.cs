71d1e5f00a71618344d4df9c41e64773d20bd09e
MagnusKochDTU <s175189@student.dtu.dk>
2019-04-03 14:27:50 +0200

    * IsDisco.csproj: * UserPrefs.xml: * project.assets.json: * IsDisco.csproj.nuget.cache: * DefaultClient.cs: * DefaultClientImpl.cs: * IsDisco.assets.cache:
    * IsDisco.csproj.nuget.g.props: Created first version of client
      communication with backend

---
+++ IsDisco/IsDisco/Rest/Implementations/DefaultClientImpl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace IsDisco.Rest.Implementations
+{
+    public abstract class UserClientImpl : Interfaces.UserClient<T>
+    {
+        private static readonly string restService = "https://localhost:5001/api/";
+        private static HttpClient httpClient = new HttpClient();
+        private String servicePath;
+
+        public UserClientImpl(string servicePath)
+        {
+            this.servicePath = servicePath;
+        }
+
+        public async System.Threading.Tasks.Task<T> Add(T element)
+        {
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync(restService + servicePath, element);
+            if (response.IsSuccessStatusCode)
+            {
+                return response.StatusCode;
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        public async System.Threading.Tasks.Task<T> Delete(int id)
+        {
+            HttpResponseMessage response = await httpClient.DeleteAsync(restService + servicePath + $"{id}");
+            return response.StatusCode;
+        }
+
+        public async System.Threading.Tasks.Task<T> Get(int id)
+        {
+            T element = null;
+            HttpResponseMessage response = await httpClient.GetAsync(restService + servicePath + $"{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                element = await response.Content.ReadAsAsync<T>();
+                return element;
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        public async System.Threading.Tasks.Task<System.Collections.Generic.List<T>> GetAll()
+        {
+            List<T> elements = new List<T>();            
+            HttpResponseMessage response = await httpClient.GetAsync(restService + servicePath);
+            if (response.IsSuccessStatusCode)
+            {
+                elements = await response.Content.ReadAsAsync<T>();
+                return elements;
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        public async System.Threading.Tasks.Task<T> Update(int id, T newElement)
+        {
+            HttpResponseMessage response = await httpClient.PutAsJsonAsync(restService + servicePath + $"{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                newElement = await response.Content.ReadAsAsync<T>();
+                return newElement;
+            }
+            else
+                throw new NotImplementedException();
+        }
+    }
+}
---
+++ IsDisco/IsDisco/Rest/Interfaces/DefaultClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IsDisco.Rest.Interfaces
+{
+    public interface UserClient<T>
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Add(T element);
+        Task<T> Update(int id, T newElement);
+        Task<T> Delete(int id);
+    }
+}
