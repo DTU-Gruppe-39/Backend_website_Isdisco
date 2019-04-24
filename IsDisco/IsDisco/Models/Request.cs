7c7ac2e5487b022794ce11ddd01203006e03a9f4
Thomas Mattsson <tmattsson0@gmail.com>
2019-04-24 16:17:55 +0200

    Merged track1 and track

--- IsDisco/IsDisco/Models/Request.cs
+++ IsDisco/IsDisco/Models/Request.cs
@@ -4,13 +4,13 @@ namespace IsDisco.Models
     public class Request
     {
         public String ReqId { get; set; }
-        public Track1 Track { get; set; }
+        public Track Track { get; set; }
         public User User { get; set; }
         public DateTime Since { get; set; }
         public int Votes { get; set; }
 
 
-        public Request(string reqId, Track1 track, User user, DateTime since)
+        public Request(string reqId, Track track, User user, DateTime since)
         {
             this.ReqId = reqId;
             this.Track = track;
