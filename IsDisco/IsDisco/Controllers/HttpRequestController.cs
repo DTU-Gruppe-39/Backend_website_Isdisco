bd566d8d42b26fac9f975c727c707fa48878ffbc
Patrick Marcell Madsen <s175209@student.dtu.dk>
2019-05-06 15:58:20 +0200

    Http Request
    Made Request match with server

---
+++ IsDisco/IsDisco/Controllers/HttpRequestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace IsDisco.Controllers
+{
+    public class HttpRequestController
+    {
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- IsDisco/IsDisco/Models/Request.cs
+++ IsDisco/IsDisco/Models/Request.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Collections;
+
 namespace IsDisco.Models
 {
     public class Request
     {
-        public String ReqId { get; set; }
+        public int ReqId { get; set; }
         public Track Track { get; set; }
-        public User User { get; set; }
+        public int UserId { get; set; }
         public DateTime Since { get; set; }
-        public int Votes { get; set; }
+        public ArrayList UpVotes { get; set; }
+        public int DownVotes { get; set; }
 
 
-        public Request(string reqId, Track track, User user, DateTime since)
+        public Request(int reqId, Track track, int user, DateTime since )
         {
             this.ReqId = reqId;
             this.Track = track;
-            this.User = user;
+            this.UserId = user;
             this.Since = since;
-            this.Votes = 1;
+            this.UpVotes = new ArrayList();
+            UpVotes.Add(reqId);
+            this.DownVotes = 0;
         }
 
         public Boolean CompareReq(Request Req)
