25a78bfb6812f008a5967584141c278328bff3af
Søren <s175202@student.dtu.dk>
2019-05-11 16:59:56 +0200

    Feedback is functioning

--- IsDisco/IsDisco/Models/FeedbackModel.cs
+++ IsDisco/IsDisco/Models/FeedbackModel.cs
@@ -8,13 +8,15 @@ namespace IsDisco.Models
         public User user { get; set; }
         public String tag { get; set;}
         public String message { get; set;}
+        public int id {get; set;}
 
 
-        public FeedbackModel(User user, String tag, String message)
+        public FeedbackModel(User user, String tag, String message, int id)
         {
             this.user = user;
             this.tag = tag;
             this.message = message;
+            this.id = id;
         }
     }
 }
